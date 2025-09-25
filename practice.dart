2c9c9879cbd5498654d311725c5972b59e3dd4d5
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-09-25 11:47:19 +0500

    Add files via upload

---
+++ practice.dart
@@ -0,0 +1,138 @@
+//import 'dart:io';
+// void main()
+// {
+//   print("Hello World");
+// }
+
+//Variables and Data Types 
+// int count = 3;
+// double size = 3.4;
+// String name = "Alice";
+// bool isAvailable = true;
+
+//This is  a single line text
+/*This 
+      is 
+         a  
+           multi-line commnet */
+
+// void main()
+// {
+//   int age = 10;
+//   if (age>=18)
+//   {
+//     print("You are an adult");
+//   }
+//   else
+//   {
+//     print("You are a child");
+//   }
+// }
+
+// //Switch Statement
+// void main()
+// {
+// int day = 2;
+// switch(day)
+// {
+// case 1:
+//       print("It's Monday");
+//       break;
+
+// case 2:
+//       print("It's Tuesday");
+//       break;
+
+// default:
+//      print("It's other day!");
+// }
+// }
+
+//Loops
+void main(){
+// for (int i=1 ; i<=5; i++)
+// {
+//   print("i=$i");
+// }
+
+// int j= 0;
+// while (j<=3)
+// {
+//   print(j);
+//   j++;
+// }
+
+
+// int x= 2;
+// do
+// {
+//   print(x);
+//   x++;
+// }
+// while (x<=2);
+
+// print( "What is you name?");
+
+// String? name = stdin.readLineSync();
+// print("Assalamualaikum $name");
+
+// print("What is you age?");
+// int ? age = int.parse(stdin.readLineSync()!);
+// print(age);
+
+
+// Lists
+List<int> nums = [1,2,3];
+nums.add(4);
+print(nums);
+
+
+
+
+
+}
+
+
+// void main() {
+// //   Map<String, int> marks = {'Maths':90,'English':80};
+// //   print(marks['Maths']);
+
+// // Set<int> uniqueNum = {1,2,3};
+// // uniqueNum.add(4);
+// // print(uniqueNum);
+
+// }
+
+// void greet(String name)
+// {
+//     print("Hello $name");
+// }
+
+// void main()
+// {
+//     greet("Abdul Rehman");
+// }
+
+//Dart Classes and Objects
+// class Car
+// {
+// String model ='';
+
+// void honk()
+// {
+//     print("Beep!");
+// }
+
+// }
+
+// void main()
+// {
+//     Car car = new Car();
+//     car.model = "Hond_city";
+//     car.honk();
+    
+// }
+
+
+
+
