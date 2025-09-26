82640f4ee95caffc74b23991d079db74abb51f70
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-09-26 22:39:17 +0500

    Add files via upload

---
+++ data_types.dart
@@ -0,0 +1,39 @@
+/*
+There are two typed programming language
+Statically Typed Language: The Type of the variable is known at compile time
+for example: C++, Java , Swift
+
+Dynamic Typed Language : The Type of a  variable is known at run time.
+For Example: Python , Rubby ,JavaScript
+
+*/
+
+main()
+{
+ /*
+   int
+   double
+   String
+   bool
+   dynamic
+ */
+ 
+ int amount1 = 100;
+ var amount2 = 200;
+
+ print('Amount1: $amount1|Amount2: $amount2\n' );
+
+ double dAmount1 = 100.11;
+ var dAmount2 = 200.22;
+ print('dAmount1: $dAmount1 | dAmount2: $dAmount2\n');
+
+ String name1 = 'Ahmad';
+ var name2 = 'Bilal';
+
+ print('My name is $name1 $name2 \n');
+
+ bool isItTrue1 = true;
+ var isItTrue2  = false;
+ 
+
+}
