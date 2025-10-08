59a9df38cb653e49e2634d3929aae5ef337c53eb
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-10-08 23:51:31 +0500

    Add files via upload

--- List.dart
+++ List.dart
@@ -12,4 +12,27 @@ void main()
     print(n);
   }
 
+  // We can also write it as, compiler will automatically recognize it.
+  var names = ['Jack','Jill'];
+  print(names);
+
+  //We can also store different data types
+  var input = ['Jack','Jill',10,100.1];
+  print(input);
+  //now this input is an object.
+  
+  //If we want List should be string type , we can do this
+  //List<String> data = ['Ali', 'Ahmad', 10 , 5]; //Error, because you cannot add data other than String 
+                                                //type here.
+  List<String> data = ['Ali','Ahmad'];
+  print(data);
+
+  //We can also change the value of any index.
+  data[1] = 'Asghar';
+  
+  //If We want to define each of thes values as constantts  
+  List<String> item = const['bread','soap'];
+  print(item);
+ 
+
 }
