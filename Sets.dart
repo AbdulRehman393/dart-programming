11f8c6ce6b267d69cf623d788ed7e0843a6ef726
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-10-09 23:08:16 +0500

    Add files via upload

---
+++ Sets.dart
@@ -0,0 +1,13 @@
+void main() {
+  // Using curly braces {}
+  var numbers = {1, 2, 3, 4};
+  print(numbers); // {1, 2, 3, 4}
+
+  // Using Set constructor
+  var names = <String>{'Eiman', 'Ali', 'Sara'};
+  print(names); // {Eiman, Ali, Sara}
+
+  // Empty set (must specify type)
+  var emptySet = <int>{};
+  print(emptySet); // {}
+}
