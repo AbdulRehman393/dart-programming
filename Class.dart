4bceb35873369afefdc373035cea68c1bf2ea047
AbdulRehmanSaeed <140309861+AbdulRehman393@users.noreply.github.com>
2025-10-13 20:51:46 +0500

    Add files via upload

--- Class.dart
+++ Class.dart
@@ -1,1 +1,27 @@
 //Class is a bluprint 
+// Defining a class
+class Car {
+  // Properties (fields)
+  String brand;
+  String color;
+  int year;
+
+  // Constructor
+  Car(this.brand, this.color, this.year);
+
+  // Method
+  void displayInfo() {
+    print("Brand: $brand, Color: $color, Year: $year");
+  }
+}
+
+// Main function (program starts here)
+void main() {
+  // Creating objects of the class
+  var car1 = Car("Toyota", "Red", 2022);
+  var car2 = Car("Honda", "Black", 2023);
+
+  // Calling method using objects
+  car1.displayInfo();
+  car2.displayInfo();
+}
